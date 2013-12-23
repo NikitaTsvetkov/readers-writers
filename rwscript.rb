9550c85d8a1b60e58f9e73bde3600d01ec1f2aa9
Nikita <vampire@vampire-pc.Dlink>
2013-12-24 01:29:46 +0400

    reading and writing works

--- rwscript.rb
+++ rwscript.rb
@@ -1,11 +1,23 @@
 require 'thread'
 
-require 'timers'
+class Timer
+  def initialize
+    puts "timer ready"
+    @should_tick = true
+  end
+  def every x , &block
+    while @should_tick
+      puts "try tick"
+      sleep(x)
+      yield block
+    end
+  end
+end
 
 class Buffer
   attr_reader :amount , :semaphore, :capacity 
-  def initialize (mutex)
-    @semaphore = mutex
+  def initialize 
+    @semaphore = Mutex.new
     @amount = 0
     @capacity = 100
   end
@@ -13,11 +25,13 @@ class Buffer
   def put
     raise "buffer overflow, please wait!" if (@amount + 1 > @capacity)
     @amount += 1
+    1
   end
   
   def get
     raise "nothing to consume, please wait!" if (@amount - 1 < 0)
     @amount -= 1
+    1
   end
 end
 
@@ -28,12 +42,19 @@ class Consumer
     @buffer = buffer
     @needed_amount = 0
     @delay = delay
-    @timer = new Timer
+    @timer = Timer.new
   end
+  
+  def start_consume
+    @timer.every(@delay){ self.consume }
+  end
+  
   def consume 
-     semaphore.synchronize do
+    puts "#{@consumer_num} consuming"
+     @buffer.semaphore.synchronize do
        begin
-         @buffer.get if @needed_amount > 0 
+         @needed_amount += 1
+         @needed_amount -= @buffer.get while @needed_amount > 0 
        rescue Exception => e
          puts e.message + " in consumer \# #{@consumer_num} , it needs #{@needed_amount} more items"
        end
@@ -44,17 +65,27 @@ end
 class Producer
   attr_reader :produced_amount
   def initialize (producer_num, buffer, delay)
+    puts "producer ready"
     @buffer = buffer
     @produced_amount = 0
     @producer_num = producer_num
     @delay = delay
+    @timer = Timer.new
   end
+  
+  def start_produce
+    @timer.every(@delay){ self.produce }
+  end
+  
   def produce
-     semaphore.synchronize do
+    puts "#{@producer_num} producing"     
+    @buffer.semaphore.synchronize do
        begin
-         @buffer.put
+         @produced_amount += 1
+         @produced_amount -= @buffer.put while @produced_amount > 0 
        rescue Exception => e
-         puts e.message + " in consumer \# #{@consumer_num}"
+         puts e.message + " in producer \# #{@producer_num} , it produced #{@producer_num} more items"
+         
        end
      end
   end
@@ -62,14 +93,22 @@ end
 
 semaphore = Mutex.new
 
-a = Thread.new {
-  semaphore.synchronize {
-    # access shared resource
-  }
-}
+buffer = Buffer.new
+consumers_threads = Array.new
+0.upto(10) do |x|
+  consumers_threads << Thread.new do
+    puts "in thread"
+    consumer = Consumer.new(x,buffer, 3)
+    consumer.start_consume
+  end
+end
 
-b = Thread.new {
-  semaphore.synchronize {
-    # access shared resource
-  }
-}
+producers_threads = Array.new
+0.upto(12) do |x|
+  producers_threads << Thread.new do
+    producer = Producer.new(x, buffer, 3)
+    producer.start_produce
+  end
+end
+
+sleep(400)
